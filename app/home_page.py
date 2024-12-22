143e2d301bacd493ec2cc65b5f1df6da2807809e
Trzopa <kuba.trzop30@gmail.com>
2024-12-22 14:49:24 +0100

    22.12

--- app/home_page.py
+++ app/home_page.py
@@ -10,10 +10,16 @@ st.write("""
     Wgraj plik CSV poniżej, a następnie przejdź do odpowiednich analiz na innych stronach.
 """)
 
+
 # Funkcja do wgrywania pliku
 def upload_file():
     uploaded_file = st.file_uploader("Wgraj plik CSV", type="csv")
     if uploaded_file is not None:
+        # Sprawdzenie rozmiaru pliku w MB
+        file_size_mb = uploaded_file.size / (1024 * 1024)
+        if file_size_mb > 1000:
+            st.error(f"Plik jest zbyt duży: {file_size_mb:.2f} MB. Maksymalny dozwolony rozmiar to 1000 MB.")
+            return
         try:
             df = pd.read_csv(uploaded_file)
             st.session_state['df_sales'] = df
@@ -21,6 +27,7 @@ def upload_file():
             st.dataframe(df.head())
         except Exception as e:
             st.error(f"Nie udało się wczytać pliku: {e}")
+
 
 # Sprawdzenie, czy plik jest już wgrany
 if 'df_sales' not in st.session_state:
